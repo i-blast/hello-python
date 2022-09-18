c6e25c1df56d8e859e2dc555f61bbd276be7d497
ilYa <i.blast59@gmail.com>
2022-09-18 04:54:24 +0300

    LF -> CRLF

--- hello-python.py
+++ hello-python.py
@@ -1,17 +1,20 @@
 import shlex
 import sys
 
+
 def echo(phrase: str) -> None:
-   """A dummy wrapper around print."""
+    """A dummy wrapper around print."""
    # for demonstration purposes, you can imagine that there is some
-   # valuable and reusable logic inside this function
-   print(phrase)
+    # valuable and reusable logic inside this function
+    print(phrase)
+
 
 def main() -> int:
     """Echo the input arguments to standard output"""
     phrase = shlex.join(sys.argv)
     echo(phrase)
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
